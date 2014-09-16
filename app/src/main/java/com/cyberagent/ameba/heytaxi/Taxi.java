3d4b80e6a9f31a257392146bb524caa047d4e9b8
Yamada <math-yamada1222@emobile.ne.jp>
2014-09-16 12:34:06 +0900

    0916

--- app/src/main/java/com/cyberagent/ameba/heytaxi/Taxi.java
+++ app/src/main/java/com/cyberagent/ameba/heytaxi/Taxi.java
@@ -1,22 +1,23 @@
 package com.cyberagent.ameba.heytaxi;
 
 
-import android.content.Context;import android.content.res.Resources;
+import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;import android.graphics.Paint;
-import android.util.AttributeSet;import android.util.Log;import android.view.MotionEvent;import android.view.View;
-
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import static android.view.MotionEvent.ACTION_DOWN;
 
-class Taxi extends View {
-    public Taxi(Context context) {
-        super(context);
-    }
-
+class Taxi {
     //class Taxi {
     Paint paint = new Paint();
-    //スタートのY座標    int playerVY = -10;  //上に10ずつ動く    int playerX; //スタートのx座標
+    //スタートのY座標    int playerVY = -10;  //上に10ずつ動く
+    int playerX; //スタートのx座標
     int playerY= 1920;  //スタートのY座標
     int playerVY;  //y方向移動量
     Bitmap taxi;
@@ -25,21 +26,11 @@ class Taxi extends View {
     int height; //タクシーの画像の高さ
     int viewWidth; //画面の幅
     int viewHeight; //画面の高さ
-
-    //5つのレーンのX座標
-    int[] lane = new int[]{0, 216, 432, 648, 846};
-
-
-    @Override
-    public void onWindowFocusChanged(boolean hasWindowFocus) {
-        super.onWindowFocusChanged(hasWindowFocus);
-        viewWidth = getWidth();
-        viewHeight = getHeight();
-
-        playerY = viewHeight;
+    int dispX = 1080; //画面幅
+    int lane ;
 
         //コンストラクタ
-        Taxi(Resources res, int playerX, int playerVY){
+        Taxi(Resources res, int playerX, int playerVY ){
             this.taxi = BitmapFactory.decodeResource(res, R.drawable.ic_launcher);
             this.width = taxi.getWidth();
             this.height = taxi.getHeight();
@@ -49,26 +40,29 @@ class Taxi extends View {
 
 
             //画像読み込みfrom SampleView  書き換え必要(constructor)
-        private void init () {
+//        private void init () {
 
 //        Resources res = this.getContext().getResources();
-            taxi = BitmapFactory.decodeResource(res, R.drawable.ic_launcher);
-            width = taxi.getWidth();
-            height = taxi.getHeight();
-        }
-        Taxi( int playerX, int playerVY){
-            this.playerX = playerX;
+//            taxi = BitmapFactory.decodeResource(res, R.drawable.ic_launcher);
+//            width = taxi.getWidth();
+//            height = taxi.getHeight();
+//        }
+       Taxi( int lane, int playerVY){
+            this.lane = lane;
+            this.playerX = lane * dispX / 5;
             this.playerVY = playerVY;
         }
 
+
+
 //    int r = new java.util.Random().nextInt(4);
-        int playerX = lane[r];
+//        int playerX = lane[r];
 
-    @Override
-    public void onDraw(Canvas c) {
-
-        //数値処理
-        playerY += playerVY;
+//    @Override
+//    public void onDraw(Canvas c) {
+//
+//        数値処理
+//        playerY += playerVY;
 /*
         //上まで行ったら下に戻る動き
         if (playerY < 0) {
@@ -78,7 +72,7 @@ class Taxi extends View {
         }
         //描画処理
         c.drawBitmap(taxi, playerX, playerY, paint);
-
+//
 
         // ループ処理、スピードの調整（ミリ秒）
         postInvalidateDelayed(50);
@@ -86,11 +80,11 @@ class Taxi extends View {
     }
 */
         //以下タッチイベントですがエラーです
-        @Override
-        public boolean onTouchEvent (MotionEvent ev){
-            float x = ev.getX();
-            float y = ev.getY();
-            String action = "";
+//        @Override
+//        public boolean onTouchEvent (MotionEvent ev){
+//            float x = ev.getX();
+//            float y = ev.getY();
+//            String action = "";
 ////    @Override
 //    public boolean TouchEvent(MotionEvent ev) {
 //        float x = ev.getX();
@@ -121,31 +115,6 @@ class Taxi extends View {
 //           // Log.d("TAG", "action : " + action + " count : " + touch_count);
 //        } else {
 //        }
-//        return true;
-//    }
 
-            if (viewWidth / 2 - width / 2 <= x && x <= viewWidth / 2 + width / 2 && playerY <= y && y <= playerY + viewHeight) {
-                switch (ev.getAction() & MotionEvent.ACTION_MASK) {
-                    case ACTION_DOWN:
-                        action = "Touch Down";
-                        //  touch_count += 1;
-//                    taxi = BitmapFactory.decodeResource(res, R.drawable.ic_launcher_reverse);
-                        playerVY = 0;
-
-                        //playerVY = -1920;                    break;
-                        // case MotionEvent.ACTION_MOVE:
-                        // action = "Touch Move";                    break;
-                        // case MotionEvent.ACTION_UP:
-                        // action = "Touch Up";//
-                        // taxi = BitmapFactory.decodeResource(res, R.drawable.ic_launcher);                    break;
-                        // case MotionEvent.ACTION_CANCEL:
-                        // action = "Touch Cancel";                    break;
-                        //            }
-                        // Log.d("TAG", "action : " + action + " count : " + touch_count);        }
-                        // else {        }        return true;    }}
-                }
-            }
-            return false;
-        }
-
-}
+//        return true;}
+    }
